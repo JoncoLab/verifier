cbefb8c24588b4c78b1bd65126eefd52662e476c
Saladin <nem97.sv@gmail.com>
2017-12-28 18:35:14 +0200

    0.0.4

--- src/App.js
+++ src/App.js
@@ -1,18 +1,21 @@
+import Header from "./components/header/header";
 import React, { Component } from 'react';
 import './styles/App.css';
-import Header from "./components/header/header";
+import {Sign} from "./components/Sign";
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-          <Header/>
-          <div id="background">
-            <img alt="background" src="./img/decor.svg"/>
-          </div>
-      </div>
-    );
-  }
+    render() {
+        return (
+            <div className="App">
+                <Header/>
+                <div id="background">
+                    <img alt="background" src="./img/decor.svg"/>
+                </div>
+                <Sign/>
+            </div>
+        );
+    }
 }
 
 export default App;
+
---
+++ src/components/Sign.js
@@ -0,0 +1,116 @@
+import React, {Component} from "react";
+import { I18n } from "react-i18next";
+
+
+export class Sign extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            type: "in"
+        };
+
+        this.switcher = this.switcher.bind(this);
+    }
+    switcher() {
+        this.setState({
+            type: this.state.type === "in" ? "up" : "in"
+        });
+    }
+    render() {
+        return (
+            <I18n ns="translations">
+                {
+                    (t) => (
+                        <section className="sign">
+                            {
+                                this.state.type === "in" ?
+                                    (
+                                        <form id="sign-in-form">
+                                            <img
+                                                className="sign-form-icon"
+                                                src="img/icon_login.svg"
+                                                alt="Sign form icon"
+                                            />
+                                            <h2 className="form-caption">{t("sign.caption")}</h2>
+                                            <input
+                                                type="email"
+                                                name="sign-in-email"
+                                                id="sign-in-email"
+                                                placeholder={t("sign.in.emailPlaceholder")}
+                                            />
+                                            <input
+                                                type="password"
+                                                name="sign-in-password"
+                                                id="sign-in-password"
+                                                placeholder={t("sign.in.passPlaceholder")}
+                                            />
+                                            <input
+                                                type="submit"
+                                                name="sign-in-submit"
+                                                id="sign-in-submit"
+                                            />
+                                            <label htmlFor="sign-in-submit">{t("sign.in.submitText")}</label>
+                                        </form>
+                                    ) :
+                                    (
+                                        <form id="sign-up-form">
+                                            <img
+                                                className="sign-form-icon"
+                                                src="img/icon_login.svg"
+                                                alt="Sign form icon"
+                                            />
+                                            <input
+                                                type="text"
+                                                name="name"
+                                                id="name"
+                                                placeholder={t("sign.up.namePlaceholder")}
+                                            />
+                                            <input
+                                                type="email"
+                                                name="sign-up-email"
+                                                id="sign-up-email"
+                                                placeholder={t("sign.up.emailPlaceholder")}
+                                            />
+                                            <input
+                                                type="password"
+                                                name="sign-up-password"
+                                                id="sign-up-password"
+                                                placeholder={t("sign.up.passPlaceholder")}
+                                            />
+                                            <input
+                                                type="password"
+                                                name="sign-up-password-confirm"
+                                                id="sign-up-password-confirm"
+                                                placeholder={t("sign.up.confirmPassPlaceholder")}
+                                            />
+                                            <input
+                                                type="submit"
+                                                name="sign-up-submit"
+                                                id="sign-up-submit"
+                                            />
+                                            <label htmlFor="sign-up-submit">{t("sign.up.submitText")}</label>
+                                        </form>
+                                    )
+                            }
+                            <div className="sign-in-with-socials">
+                                <button className="fb"/>
+                                <button className="tw"/>
+                            </div>
+                            <button
+                                className="switch-link"
+                                onClick={this.switcher}>
+                                {
+                                    t(this.state.type === "in" ?
+                                        "sign.in.switchLinkText" :
+                                        "sign.up.switchLinkText"
+                                    )
+                                }
+                            </button>
+                        </section>
+                    )
+                }
+            </I18n>
+        );
+    }
+}
