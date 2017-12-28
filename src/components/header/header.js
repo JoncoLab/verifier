bedddf0cc826007d45e30bd176737bb10d6cfd2e
princeShozanah <max.pechko@gmail.com>
2017-12-28 18:33:36 +0200

    0.0.3

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,26 +1,30 @@
 import React, {Component} from 'react';
+import {Col} from 'react-bootstrap';
+import {Row} from 'react-bootstrap';
 
 class Header extends Component {
     render() {
         return (
             <header className="navbar">
-                <div className="navbar-left">
-                    <button className="download-app">Download App</button>
-                    <span className="lang">
+                <Row bsClass="header-row">
+                    <Col xs={5} xsPush={0.5} md={3} mdPush={1} className="navbar-left">
+                        <button className="download-app">Скачать Приложение</button>
+                        <span className="lang">
                         <span id="ru">RU</span>
                         <span id="en">EN</span>
                     </span>
-                </div>
-                <div className="logo">
-                    <img alt="logo" src="../../img/logo.png"/>
-                </div>
-                <div className="navbar-right">
-                    <span>FirstName</span>
-                    <span>LastName</span>
-                    <div className="profile-photo">
-                        <img alt="profile photo" src="../../img/user-login.svg"/>
-                    </div>
-                </div>
+                    </Col>
+                    <Col xs={4} md={4} mdOffset={1} className="navbar-center">
+                        <img alt="logo" src="../../img/logo.png"/>
+                    </Col>
+                    <Col xs={3} xsPull={2} md={2} mdPull={1} className="navbar-right">
+                        <span>FirstName</span>
+                        <span>LastName</span>
+                        <div className="profile-photo">
+                            <img alt="profile photo" src="../../img/user-login.svg"/>
+                        </div>
+                    </Col>
+                </Row>
             </header>
         );
     }
