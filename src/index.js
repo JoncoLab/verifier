d177e5d516253d99f9423a8bfe980d9d0b8dd256
Saladin <nem97.sv@gmail.com>
2018-01-13 18:25:01 +0200

    0.2.9

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import './styles/index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 import i18n from "i18next";
